1b3b00b556b25aa3418e355de0cb116d6ce1697f
Srishanth <srishanth471011@gmail.com>
2024-11-02 00:11:01 +0530

    Added files and updated files

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+const {nextui} = require("@nextui-org/react");
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
